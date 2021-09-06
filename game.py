0ec76280f9fdef392b11db48d7f8fc69a98e5343
Felipe <felipe@superclient.com.br>
2021-09-05 21:58:12 -0300

    Adicionando inimigos

--- game.py
+++ game.py
@@ -9,10 +9,13 @@ class Game:
         self.win = win
         self.bg = pygame.image.load(os.path.join('imagens', 'fundo.png'))
         self.bg = pygame.transform.scale(self.bg, (self.w, self.h))
-        self.gatos = []
+        gato = Gato()
+        self.gatos: list[Gato] = [gato]
 
     def draw(self):
         self.win.blit(self.bg, (0,0))
+        for gato in self.gatos:
+            gato.draw(win)
 
         pygame.display.update()
         
@@ -21,7 +24,7 @@ class Game:
         clock = pygame.time.Clock()
         
         while run:
-            clock.tick(500)
+            clock.tick(60)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     run = False
--- inimigos/inimigo.py
+++ inimigos/inimigo.py
@@ -1,8 +1,10 @@
 import pygame
 import os
+import math
 
 VERMELHO = (255, 0, 0)
 VERMELHO2 = (240, 0, 0)
+
 
 class Inimigo:
     def __init__(self):
@@ -10,13 +12,17 @@ class Inimigo:
         self.height = 64
         self.animation_count = 0
         self.images = []
-        self.image = pygame.image.load(os.path.join('imagens','h1.png')).convert_alpha()
+        self.image = pygame.image.load(
+            os.path.join("imagens", "ren.png")
+        ).convert_alpha()
         self.health = 1
         self.max_health = 50
         self.vel = 3
-        self.path = [(10,10), (20,20), (30,30), (40,40),]
+        self.path = [(100, 100), (200, 200), (300, 300), (400, 400)]
         self.path_pos = 0
-    
+        self.x = self.path[0][0]
+        self.y = self.path[0][1]
+
     def draw(self, win):
         """
         Desenha o inimigo na tela
@@ -25,8 +31,14 @@ class Inimigo:
             win: Janela
         """
         self.img = self.images[self.animation_count]
-        win.blit(self.img, (self.x - self.img.get_width()/2, self.y- self.img.get_height()/2 - 35))
-        
+        win.blit(
+            self.img,
+            (
+                self.x - self.img.get_width() / 2,
+                self.y - self.img.get_height() / 2 - 35,
+            ),
+        )
+
     def health_bar(self, win):
         """Desenha a barra de vida dos inimigos"""
         pass
@@ -35,13 +47,9 @@ class Inimigo:
         """Move o inimigo"""
         self.animation_count += 1
         if self.animation_count >= len(self.images):
-            self.animation_count = 0 
-        
-        x1, y1 = self.path[self.path_pos]
+            self.animation_count = 0
+
         if self.path_pos + 1 >= len(self.path):
-            x2, y2 = (-10, 355)
+            self.x, self.y = (-10, 355)
         else:
-            x2, y2 = self.path[self.path_pos+1]
-        
-        
-        
+            self.x, self.y = self.path[self.path_pos + 1]
